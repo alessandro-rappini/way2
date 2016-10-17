928336bad0fa9466b04816da7b7f15c3af25c389
alessandro-rappini <rappini.alessandro@hotmail.it>
2016-10-17 22:21:38 +0200

    Prova 1

--- app/src/main/java/com/example/alessandrorappini/way/MainActivity.java
+++ app/src/main/java/com/example/alessandrorappini/way/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import com.example.alessandrorappini.way.Interazione.MenuInterazione;
 
 public class MainActivity extends AppCompatActivity {
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
